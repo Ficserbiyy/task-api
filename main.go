b02bf8fdb93af5e275c9d61b883a4546ecd9a637
ficserbiYY <ficserbiy@gmail.com>
2026-09-03 17:24:50 +0500

    build: initialize the HTTP server and http-swagger

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
+// @title Task API
+// @description A simple task API written in Go.
+// @host 127.0.0.1:8080
+// @BasePath /
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	_ "github.com/Ficserbiyy/task-api/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
+)
 
 func main() {
-	fmt.Println()
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/swagger/", httpSwagger.WrapHandler)
+
+	log.Println("Server listening on http://127.0.0.1:8080")
+
+	if err := http.ListenAndServe("127.0.0.1:8080", mux); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
